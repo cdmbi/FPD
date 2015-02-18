602d48b74ed12b203163b6b50ba1f921245840ef
Likit Preeyanon <preeyano@msu.edu>
2015-02-18 14:58:29 +0700

    added sliders for min,max excitation and emission

--- source/fpdapp.py
+++ source/fpdapp.py
@@ -10,13 +10,15 @@ import pandas as pd
 import numpy as np
 
 from collections import OrderedDict
-from bokeh.plotting import circle, figure
+from bokeh.plotting import circle, figure, curdoc
 from bokeh.models import Plot, ColumnDataSource, Range1d, HoverTool
 from bokeh.properties import Instance, String
 from bokeh.server.app import bokeh_app
 from bokeh.server.utils.plugins import object_page
-from bokeh.models.widgets import HBox, Slider, TextInput, VBox,\
-                                    VBoxForm, Select, PreText, DataTable
+from bokeh.models.widgets import (HBox, Slider, TextInput,
+                                    VBox, VBoxForm, Select,
+                                    PreText, DataTable,
+                                )
 from bokeh.models.widgets.tables import TableColumn
 
 _data = pd.read_csv(os.path.join('data',
@@ -42,10 +44,12 @@ data = _data.ix[d.index,['emission_new',
                         'pka',
                         'protein_name',
                         'amino_acid_sequence',
+                        'doi',
                         ]].fillna('')
 
 CHROMOPHORES = sorted(set(data['chromophore_class']))
-xy_margin = 50
+CHROMOPHORES.insert(0, 'All')
+xy_margin = 80
 max_emission = max(data['emission_new']) + xy_margin
 min_emission = min(data['emission_new']) - xy_margin
 max_excitation = max(data['excitation_new']) + xy_margin
@@ -53,6 +57,8 @@ min_excitation = min(data['excitation_new']) - xy_margin
 
 columns = [
         TableColumn(field='fpid', title='FPID'),
+        TableColumn(field='chromophore_name', title='chromophore_name'),
+        TableColumn(field='chromophore_class', title='chromophore_class'),
         TableColumn(field='protein_name', title='Protein name'),
         TableColumn(field='excitation_new', title='Excitation'),
         TableColumn(field='emission_new', title='Emission'),
@@ -74,16 +80,19 @@ class FPDApp(HBox):
     plot_frame = Instance(HBox)
     data_table = Instance(DataTable)
 
-    # text = Instance(TextInput)
-
-    excitation = Instance(Slider)
-    emission = Instance(Slider)
+    # widget instances
+    # TODO:: add a reset button
+    min_excitation = Instance(Slider)
+    max_excitation = Instance(Slider)
+    min_emission = Instance(Slider)
+    max_emission = Instance(Slider)
     chrom_class_select = Instance(Select)
-    chrom_class = String(default='AYG')
+    chrom_class = String(default='All')
 
     plot = Instance(Plot)
     source = Instance(ColumnDataSource)
-    pretext = Instance(PreText)
+    # pretext = Instance(PreText)
+
 
     def __init__(self, *args, **kwargs):
         super(FPDApp, self).__init__(*args, **kwargs)
@@ -93,40 +102,91 @@ class FPDApp(HBox):
     def create(cls):
         obj = cls()
 
-        obj.make_inputs()
-        obj.source = ColumnDataSource(data=obj.get_data())
-        obj.pretext = PreText(text="No items selected.", width=500)
+        # create input widgets only once
+        obj.min_excitation = Slider(
+                title="Min Excitation", name="min_excitation",
+                value=min_excitation,
+                start=min_excitation,
+                end=max_excitation,
+                )
+        obj.max_excitation = Slider(
+                title="Max Excitation", name="max_excitation",
+                value=max_excitation,
+                start=min_excitation,
+                end=max_excitation,
+                )
+        obj.min_emission = Slider(
+                title="Min Emission", name="min_emission",
+                value=min_emission,
+                start=min_emission,
+                end=max_emission,
+                )
+        obj.max_emission = Slider(
+                title="Max Emission", name="max_emission",
+                value=max_emission,
+                start=min_emission,
+                end=max_emission,
+                )
+
+        obj.chrom_class_select = Select(
+                name="Chromophore",
+                value='All',
+                options=CHROMOPHORES,
+                )
+
+        obj.source = ColumnDataSource(data=data)
         obj.data_table = DataTable(source=obj.source, columns=columns)
-        obj.data_table.width = 1000
+        obj.data_table.width = 1200
         obj.data_table.source = obj.source
+        # obj.pretext = PreText(text='No selected items', width=400)
+
         obj.make_plots()
         obj.set_children()
 
+
         return obj
 
-
-    def make_inputs(self):
-        print('CALL: make_inputs')
-        print('Current chromophore class: %s', self.chrom_class)
-
-        self.excitation = Slider(
-                title="Excitation", name="excitation",
-                value=1.0, start=-5.0, end=5.0,
+    def reset_sliders(self):
+        self.min_excitation = Slider(
+                title="Min Excitation", name="min_excitation",
+                value=self.min_excitation.value,
+                start=min_excitation,
+                end=max_excitation,
                 )
-        self.emission = Slider(
-                title="Emission", name="emission",
-                value=1.0, start=-5.0, end=5.0,
+        self.max_excitation = Slider(
+                title="Max Excitation", name="max_excitation",
+                value=self.max_excitation.value,
+                start=min_excitation,
+                end=max_excitation,
                 )
-        self.chrom_class_select = Select(
-                name="Chromophore",
-                value='AYG',
-                options=CHROMOPHORES,
+        self.min_emission = Slider(
+                title="Min Emission", name="min_emission",
+                value=self.min_emission.value,
+                start=min_emission,
+                end=max_emission,
+                )
+        self.max_emission = Slider(
+                title="Max Emission", name="max_emission",
+                value=self.max_emission.value,
+                start=min_emission,
+                end=max_emission,
                 )
 
 
     def get_data(self):
-        selected = data[data['chromophore_class']==self.chrom_class]
-        return selected
+        df = data
+
+        if self.chrom_class == 'All':  # all chromophore classes
+            return df
+        else:
+            df = df[df['chromophore_class']==self.chrom_class]
+
+        df = df[df['excitation_new']>=self.min_excitation.value]
+        df = df[df['excitation_new']<=self.max_excitation.value]
+        df = df[df['emission_new']>=self.min_emission.value]
+        df = df[df['emission_new']<=self.max_emission.value]
+
+        return df
 
 
     def make_source(self):
@@ -134,32 +194,37 @@ class FPDApp(HBox):
 
 
     def make_plots(self):
-        print('CALL: make_plots')
+        # print('CALL: make_plots')
         self.plot = self.scatter_plot()
 
     @property
     def selected_df(self):
-        pandas_df = self.get_data()
+        df = data
         selected = self.source.selected
         if selected:
-            pandas_df = pandas_df.iloc[selected, :]
-        return pandas_df
+            df = df.iloc[selected, :]
+        return df
 
 
     def scatter_plot(self):
         toolset = "pan,reset,resize,save,wheel_zoom,hover,select"
 
-        plot = figure(title="A Scatter Plot", tools=toolset)
+        plot = figure(tools=toolset)
         plot.scatter('excitation_new', 'emission_new',
                 source=self.source,
-                plot_width=600, plot_height=600,
-                radius=4, fill_alpha=0.6,
+                plot_width=500, plot_height=600,
+                radius=4, fill_alpha=0.4,
                 fill_color='excitation_color_new',
                 line_color='#000000',
+                ylabel='Emission',
+                xlabel='Excitation',
                 )
+
         plot.x_range = Range1d(start=min_excitation, end=max_excitation)
         plot.y_range = Range1d(start=min_emission, end=max_excitation)
+
         hover = plot.select(dict(type=HoverTool))
+
         hover.tooltips = [
             ("FPID ", "@fpid"),
             ("Chromophore name ", "@chromophore_name"),
@@ -174,8 +239,10 @@ class FPDApp(HBox):
 
     def set_children(self):
         self.input_frame = VBoxForm(children=[
-                                self.excitation,
-                                self.emission,
+                                self.min_excitation,
+                                self.max_excitation,
+                                self.min_emission,
+                                self.max_emission,
                                 self.chrom_class_select,
                             ])
 
@@ -185,25 +252,60 @@ class FPDApp(HBox):
         self.main_frame = VBox(children=[self.top_frame, self.table_frame])
         self.children = [self.main_frame]
 
+
     def setup_events(self):
         super(FPDApp, self).setup_events()
 
         if self.source:
             self.source.on_change('selected', self, 'input_change')
+
+        if self.min_excitation:
+            self.min_excitation.on_change('value', self, 'input_change')
+
+        if self.max_excitation:
+            self.max_excitation.on_change('value', self, 'input_change')
+
+        if self.min_emission:
+            self.min_emission.on_change('value', self, 'input_change')
+
+        if self.max_emission:
+            self.max_emission.on_change('value', self, 'input_change')
+
         if self.chrom_class_select:
             self.chrom_class_select.on_change(
-                    'value', self, 'input_change')
+                                'value', self, 'input_change')
+
 
     def input_change(self, obj, attrname, old, new):
-        print('CALL: input_change')
         if obj == self.chrom_class_select:
             self.chrom_class = new
-            print '\n\nChromophore class: %s\n' % \
-                    self.chrom_class
+
+        if obj == self.min_excitation:
+            self.min_excitation.value = new
+            if self.min_excitation.value > self.max_excitation.value:
+                self.min_excitation.value = self.max_excitation.value
+
+        if obj == self.max_excitation:
+            self.max_excitation.value = new
+            if self.max_excitation.value < self.min_excitation.value:
+                self.max_excitation.value = self.min_excitation.value
+
+        if obj == self.min_emission:
+            self.min_emission.value = new
+            if self.min_emission.value > self.max_emission.value:
+                self.min_emission.value = self.max_emission.value
+
+        if obj == self.max_emission:
+            self.max_emission.value = new
+            if self.max_emission.value < self.min_emission.value:
+                self.max_emission.value = self.min_emission.value
 
         self.make_source()
-        self.pretext.text = str(self.selected_df[:2])
+        # self.pretext.text = str(self.selected_df['doi'])
         self.data_table.source = self.source
+        self.reset_sliders()
+        self.set_children()
+        curdoc().add(self)
 
 @bokeh_app.route("/bokeh/fpd/")
 @object_page("fpd")
